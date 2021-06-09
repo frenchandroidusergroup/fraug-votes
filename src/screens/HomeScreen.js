9c37f1f137ccbc94e512e0b22d5db4a5f86d2d75
Fanny Demey <fanny.demey@smartmacadam.com>
2021-06-09 21:08:14 +0200

    fix : move route redirection in onClick method

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,16 +3,20 @@ import { Button, Container, Typography } from '@material-ui/core'
 import { Link as RouterLink } from 'react-router-dom'
 import { DispatchContext, StateContext } from '../AppContext'
 import { createNewGame } from '../firebase/firebase'
+import { useHistory } from "react-router-dom";
 
 export const HomeScreen = () => {
     const state = useContext(StateContext)
     const dispatch = useContext(DispatchContext);
+    const history = useHistory();
+
 
     const firstSpeakerId = state.speakersOrder[state.speakersOrderCurrentIndex]
     const firstSpeaker = state.speakers[firstSpeakerId].name
 
-    const startNewGame = () => {
-        createNewGame(dispatch)
+    const startNewGame = async () => {
+        await createNewGame(dispatch)
+        history.push(`/counter/${firstSpeakerId}`)
     }
 
 
@@ -20,7 +24,7 @@ export const HomeScreen = () => {
         <Typography variant='h1'>Question pour des grenouilles</Typography>
 
         <h2>{firstSpeaker} commence</h2>
-        <Button variant="contained" color="primary" size="large" component={RouterLink} onClick={startNewGame} to={`/counter/${firstSpeakerId}`}>C'est parti !</Button>
+        <Button variant="contained" color="primary" size="large" component={RouterLink} onClick={startNewGame} >C'est parti !</Button>
     </Container>
 
 }
